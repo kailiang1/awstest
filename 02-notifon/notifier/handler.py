dfc011ea5e83dfa472afec8b01175c41336d0e54
kailiang1 <kaiyanliang8@gmail.com>
2019-10-21 08:56:48 -0700

    add cloudwatch post to slack

--- 02-notifon/notifier/handler.py
+++ 02-notifon/notifier/handler.py
@@ -1,8 +1,14 @@
-import json
+import os
+import requests
 
 
-def hello(event, context):
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
+def post_to_slack(event, context):
+    slack_webhook = os.environ['SLACK_WEBHOOK_URL']
+
+    slack_mesg = "From {source} at {detail[StartTime]}: {detail[Description]}".format(**event)
+    data = { "text": slack_mesg }
+    requests.post(slack_webhook, json=data)
+
+    print(slack_webhook)
+    print(event)
+    return 
--- 02-notifon/post_slack.py
+++ 02-notifon/post_slack.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import requests
-url ='https://hooks.slack.com/services/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'
-data = { "text": "Hello from python" }
-requests.post(url, json=data)
-get_ipython().run_line_magic('save', 'post_slack.py')
+url ='https://hooks.slack.com/services/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'
+data = { "text": "2Hello from python" }
+response = requests.post(url, json=data)
+print(f"Response : {response}")
