e281b50988e40f472d2264387c612baf0b9e2385
kailiang1 <kaiyanliang8@gmail.com>
2019-10-22 08:53:59 -0700

    Add IAM roles and SNS handling

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,5 +1,6 @@
 import urllib
 import boto3
+import os
 
 def start_label_detection(bucket, key): 
     rk_client = boto3.client('rekognition')
@@ -9,7 +10,13 @@ def start_label_detection(bucket, key):
                 'Bucket': bucket, 
                 'Name': key
             }
-        })
+        },
+        NotificationChannel={
+            'SNSTopicArn': os.environ['REKOGNITION_SNS_TOPIC_ARN'],
+            'RoleArn': os.environ['REKOGNITION_ROLE_ARN']
+        }
+        
+        )
     print("RK response: {}".format(response))
     return
 
@@ -21,5 +28,10 @@ def start_processing_video(event, context):
             record['s3']['bucket']['name'], 
             urllib.parse.unquote_plus(record['s3']['object']['key'])
         )
+
+    return
+
+def handle_label_detection(event, context): 
+    print("SNS event: ".format(event))
 
     return
