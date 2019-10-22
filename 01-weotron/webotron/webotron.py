c766b3a152e6ae73fee9c04993ed84c733a31fef
kailiang1 <kaiyanliang8@gmail.com>
2019-10-21 18:47:10 -0700

    Set up serverless project

--- 01-weotron/webotron/webotron.py
+++ 01-weotron/webotron/webotron.py
@@ -1,47 +1,62 @@
-# coding: utf-8
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Webotron: Deplly Website with AWS.
+Webotron automats the process of deploying static web site
+- configure aws s3buckets
+    - create them
+    - set them up for static website hosting
+    - Deploy local files to s3 buckets
+- Configure DNS with aWS route 53
+- Configure a content delivery network and ssl
+"""
+
+import mimetypes
+from pathlib import Path
+from botocore.exceptions import ClientError
+import click
 import boto3
 session = boto3.Session(profile_name='kliang-strict')
-s3 = session.resource ('s3')
+s3 = session.resource('s3')
 
-import click
-from botocore.exceptions import ClientError
-from pathlib import Path
-import mimetypes
 
 @click.group()
 def cli():
-    "webotron deploys websites to AWS"
+    """webotron deploys websites to AWS"""
     pass
+
 
 @cli.command('list_bucket_objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
-    "List bucket objects"
-    for obj in s3.Bucket(bucket).objects.all(): 
+    """List bucket objects"""
+    for obj in s3.Bucket(bucket).objects.all():
         print(obj)
 
 
 @cli.command('list_buckets')
 def list_buckets():
-    "List all s3 buckets"
+    """List all s3 buckets"""
     for bucket in s3.buckets.all():
         print(bucket)
 
+
 @cli.command('setup-bucket')
 @click.argument('bucket')
-def setup_bucket(bucket): 
-    "Create and configure S3 bucket" 
+def setup_bucket(bucket):
+    """Create and configure S3 bucket"""
     s3_bucket = None
-    try: 
+    try:
         s3_bucket = s3.create_bucket(
-            Bucket=bucket, 
-            CreateBucketConfiguration={'LocationConstraint': session.region_name}
+            Bucket=bucket,
+            CreateBucketConfiguration={
+                'LocationConstraint': session.region_name}
         )
-    except ClientError as err: 
-        if err.response['Error']['Code'] == 'BucketAlreadyOwnedByYou': 
+    except ClientError as err:
+        if err.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
             s3_bucket = s3.Bucket(bucket)
             print("Bucket {} already exists".format(bucket))
-        else: 
+        else:
             raise err
 
     policy = """
@@ -69,12 +84,12 @@ def setup_bucket(bucket):
             'Suffix': 'index.html'
         }})
     url = "http://%s.s3-website.us-east-2.amazonaws.com" % s3_bucket.name
-    print(f"url = {url}") 
+    print(f"url = {url}")
 
     return
 
 
-def upload_file(s3_bucket, path, key): 
+def upload_file(s3_bucket, path, key):
     content_type = mimetypes.guess_type(key)[0] or 'text/plain'
 
     s3_bucket.upload_file(
@@ -91,23 +106,25 @@ def upload_file(s3_bucket, path, key):
 @cli.command('sync')
 @click.argument('pathname', type=click.Path(exists=True))
 @click.argument('bucket')
-def sync(pathname,bucket): 
-    "Sync contents of PATHNAME to Bucket" 
-    
-    s3_bucket= s3.Bucket(bucket)
-    
-    root = Path(pathname).expanduser().resolve() 
+def sync(pathname, bucket):
+    """Sync contents of PATHNAME to Bucket"""
+
+    s3_bucket = s3.Bucket(bucket)
+
+    root = Path(pathname).expanduser().resolve()
     print("root = {}".format(root))
 
-    def handle_directory(target): 
-        for p in target.iterdir(): 
-            if p.is_dir(): handle_directory(p)
-            if p.is_file(): upload_file(s3_bucket, str(p), str(p.relative_to(root)))
-
+    def handle_directory(target):
+        for p in target.iterdir():
+            if p.is_dir():
+                handle_directory(p)
+            if p.is_file():
+                upload_file(s3_bucket, str(p), str(p.relative_to(root)))
 
     handle_directory(root)
 
+
 if __name__ == '__main__':
-   # list_buckets()
+    # list_buckets()
     cli()
     print("hello world")
